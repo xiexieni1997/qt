1d8c91c98a8893df27ca0923e8a0be7b54911aa7
xiexieni1997 <1394130864@qq.com>
2023-04-21 15:05:05 +0800

    123
    Signed-off-by: xiexieni1997 <1394130864@qq.com>

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QApplication>
+
+#include <QWidget>
+
+#include <QPushButton>
+
+#include <iostream>
+
+class MyWidget : public QWidget {
+public:
+    MyWidget(QWidget *parent = nullptr) : QWidget(parent) {
+        button = new QPushButton(" touch me", this);
+        connect(button, &QPushButton::clicked, this, &MyWidget::on_button_clicked);
+    }
+
+protected:
+    void on_button_clicked() {
+        std::cout << " test success" << std::endl;
+    }
+
+private:
+    QPushButton *button;
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MyWidget widget;
+    widget.show();
+    return app.exec();
+}
